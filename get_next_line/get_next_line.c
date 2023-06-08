1b7d5d5680f464b11b485dcce6c90fd9a1d2405d
Iva <104653667+pin3dev@users.noreply.github.com>
2023-06-08 18:16:46 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,9 +3,9 @@
 
 char *get_next_line(int fd)
 {
-    int     c = 0;
-    int     rd = 0;
-    char    character;
+    int     i = 0;
+    int     rd;
+    char    c;
     char    *line = malloc(100000);
     
     if (BUFFER_SIZE <= 0)
@@ -14,23 +14,23 @@ char *get_next_line(int fd)
         return (NULL);
     }
 
-    rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1);
+    rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1);
     
     while (rd > 0)
     {
-        line[c++] = character;
-        if (character == '\n')
+        line[i] = c;
+        i++;
+        if (c == '\n')
             break ;
-        rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1);
+        rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1);
     }
     
-    line[c] =  '\0';
+    line[i] =  '\0';
     
-    if (rd == -1 || c == 0 || (!line[c - 1] && !rd))
+    if (rd == -1 || i == 0 || (!line[i - 1] && !rd))
     {
         free(line);
         return (NULL);
     }
-    
     return(line);
 }
